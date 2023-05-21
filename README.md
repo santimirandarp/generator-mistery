193df0cadacfa7860b12ee70df9f2a1a5fc9ace3
Santi Miranda <santimir@protonmail.com>
2023-05-21 18:12:28 +0100

    simplify

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Scaffold a simple Typescript project using Vitest
 
 ```
-npm i yo -g
-yo:simple
+npm i yo generator-simple -g
+yo simple
 ```
 
--- app/index.js
+++ app/index.js
@@ -1,48 +1,48 @@
-"use strict";
-import cp from "child_process";
+'use strict';
+import cp from 'child_process';
 
-import camelCase from "camelcase";
-import Generator from "yeoman-generator";
+import camelCase from 'camelcase';
+import Generator from 'yeoman-generator';
 
-let username = " ";
+let username = ' ';
 
 try {
-  username = cp.execSync("git config user.name").toString();
+  username = cp.execSync('git config user.name').toString();
 } catch (e) {
   /* istanbul ignore next */
-  console.error("Missing git configuration");
+  console.error('Missing git configuration');
 }
 
 export default class extends Generator {
   async prompting() {
     const prompts = [
       {
-        type: "input",
-        name: "name",
-        message: "Your project name",
+        type: 'input',
+        name: 'name',
+        message: 'Your project name',
         default: this.appname, // Default to current folder name
       },
       {
-        type: "input",
-        name: "org",
-        message: "GitHub organization",
+        type: 'input',
+        name: 'org',
+        message: 'GitHub organization',
         default: username,
       },
       {
-        type: "input",
-        name: "userName",
-        message: "Your name",
+        type: 'input',
+        name: 'userName',
+        message: 'Your name',
         default: username.substring(0, username.length - 1),
       },
       {
-        type: "input",
-        name: "description",
-        message: "Your package description",
+        type: 'input',
+        name: 'description',
+        message: 'Your package description',
       },
       {
-        type: "confirm",
-        name: "node",
-        message: "Is it a Node.js-only library?",
+        type: 'confirm',
+        name: 'node',
+        message: 'Is it a Node.js-only library?',
         default: false,
       },
     ];
@@ -56,7 +56,7 @@ export default class extends Generator {
     const month = date.getMonth();
     const year = date.getFullYear();
     const camelName = camelCase(this.props.name);
-    const prefix = this.props.org === "mljs" ? "ml-" : "";
+    const prefix = this.props.org === 'mljs' ? 'ml-' : '';
     const includes = {
       npmName: prefix + this.props.name,
       name: this.props.name,
@@ -64,78 +64,78 @@ export default class extends Generator {
       userName: this.props.userName,
       notOnlyNode: !this.props.node,
       description: this.props.description,
-      date: year + "-" + month + "-" + day,
+      date: year + '-' + month + '-' + day,
       year: year,
       camelName: camelName,
     };
     this.fs.copy(
-      this.templatePath("tsconfig.json"),
-      this.destinationPath(".config/tsconfig.json")
+      this.templatePath('tsconfig.json'),
+      this.destinationPath('.config/tsconfig.json'),
     );
     this.fs.copy(
-      this.templatePath("tsconfig.cjs.json"),
-      this.destinationPath(".config/tsconfig.cjs.json")
+      this.templatePath('tsconfig.cjs.json'),
+      this.destinationPath('.config/tsconfig.cjs.json'),
     );
     if (includes.notOnlyNode) {
       this.fs.copy(
-        this.templatePath("tsconfig.esm.json"),
-        this.destinationPath(".config/tsconfig.esm.json")
+        this.templatePath('tsconfig.esm.json'),
+        this.destinationPath('.config/tsconfig.esm.json'),
       );
     }
     this.fs.copy(
-      this.templatePath("eslintrc.yml"),
-      this.destinationPath(".config/.eslintrc.yml")
+      this.templatePath('eslintrc.yml'),
+      this.destinationPath('.config/.eslintrc.yml'),
     );
     this.fs.copy(
-      this.templatePath("index.ts"),
-      this.destinationPath("src/index.ts")
+      this.templatePath('index.ts'),
+      this.destinationPath('src/index.ts'),
     );
     this.fs.copy(
-      this.templatePath("index.test.ts"),
-      this.destinationPath("src/__tests__/index.test.ts")
+      this.templatePath('index.test.ts'),
+      this.destinationPath('src/__tests__/index.test.ts'),
     );
     this.fs.copy(
-      this.templatePath("prettierrc.json"),
-      this.destinationPath(".config/.prettierrc.json")
+      this.templatePath('prettierrc.json'),
+      this.destinationPath('.config/.prettierrc.json'),
     );
     this.fs.copy(
-      this.templatePath("vite.config.ts"),
-      this.destinationPath(".config/vite.config.ts")
+      this.templatePath('vite.config.ts'),
+      this.destinationPath('.config/vite.config.ts'),
     );
     // templates use the ejs `includes` object i.e `<%= includes.name %>`
     this.fs.copyTpl(
-      this.templatePath("gitignore"),
-      this.destinationPath(".gitignore"),
-      includes
+      this.templatePath('gitignore'),
+      this.destinationPath('.gitignore'),
+      includes,
     );
     this.fs.copyTpl(
-      this.templatePath("LICENSE"),
-      this.destinationPath("LICENSE"),
-      includes
+      this.templatePath('LICENSE'),
+      this.destinationPath('LICENSE'),
+      includes,
     );
     this.fs.copyTpl(
-      this.templatePath("README.md"),
-      this.destinationPath("README.md"),
-      includes
+      this.templatePath('README.md'),
+      this.destinationPath('README.md'),
+      includes,
     );
     this.fs.copyTpl(
-      this.templatePath("package"),
-      this.destinationPath("package.json"),
-      includes
+      this.templatePath('package'),
+      this.destinationPath('package.json'),
+      includes,
     );
   }
 
   install() {
     let deps = [
-      "@types/jest",
-      "eslint",
-      "eslint-config-cheminfo-typescript",
-      "vitest",
-      "prettier",
-      "rimraf",
-      "typescript",
+      '@types/jest',
+      'eslint',
+      'eslint-config-cheminfo-typescript',
+      'vitest',
+      'prettier',
+      'rimraf',
+      'typescript',
     ];
 
-    cp.spawnSync("npm", ["install", "--save-dev", ...deps]);
+    cp.spawnSync('npm', ['install', '--save-dev', ...deps]);
   }
 }
