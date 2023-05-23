98c25598fe9fe0cd13eb021b24220aba279ab393
Santi Miranda <santimir@protonmail.com>
2023-05-23 19:55:51 +0100

    chore: lint

--- generators/app/toCopy/src/index.test.ts
+++ generators/app/toCopy/src/index.test.ts
@@ -1,7 +1,7 @@
-import { myModule } from ".";
+import { myModule } from '.';
 
-describe("test myModule", () => {
-  it("should return 42", () => {
+describe('test myModule', () => {
+  it('should return 42', () => {
     expect(myModule()).toBe(42);
   });
 });
