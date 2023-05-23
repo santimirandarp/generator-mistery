6ce01c9ec472cc9ffab9645fc91f3dc869be74a0
Santi Miranda <santimir@protonmail.com>
2023-05-23 19:55:35 +0100

    fix!: several directories and configration, use EJS now.

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,54 +1,55 @@
-import { join, dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
+import { join, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
 
-import camelCase from 'camelcase';
-import Generator from 'yeoman-generator';
+import camelCase from "camelcase";
+import Generator from "yeoman-generator";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const deps = [
-  '@types/jest',
-  'eslint',
-  'eslint-config-cheminfo-typescript',
-  'vitest',
-  'prettier',
-  'rimraf',
-  'typescript',
+  "@types/jest",
+  "eslint",
+  "eslint-config-cheminfo-typescript",
+  "vitest",
+  "prettier",
+  "rimraf",
+  "typescript",
+  "@vitest/coverage-c8",
 ];
 
-const toCopy = join(__dirname, 'toCopy');
-const templates = join(__dirname, 'templates');
+const toCopy = join(__dirname, "toCopy");
+const templates = join(__dirname, "templates");
 
 export default class extends Generator {
   async prompting() {
     const prompts = [
       {
-        type: 'input',
-        name: 'name',
-        message: 'Your project name',
+        type: "input",
+        name: "name",
+        message: "Your project name",
         default: this.determineAppname(),
       },
       {
-        type: 'input',
-        name: 'org',
-        message: 'GitHub organization',
+        type: "input",
+        name: "org",
+        message: "GitHub organization",
         default: await this.user.github.username(),
       },
       {
-        type: 'input',
-        name: 'userName',
-        message: 'Your name',
+        type: "input",
+        name: "userName",
+        message: "Your name",
         default: this.user.git.name(),
       },
       {
-        type: 'input',
-        name: 'description',
-        message: 'Your package description',
+        type: "input",
+        name: "description",
+        message: "Your package description",
       },
       {
-        type: 'confirm',
-        name: 'node',
-        message: 'Is it a Node.js-only library?',
+        type: "confirm",
+        name: "node",
+        message: "Is it a Node.js-only library?",
         default: false,
       },
     ];
@@ -61,7 +62,7 @@ export default class extends Generator {
     const month = date.getMonth();
     const year = date.getFullYear();
     const camelName = camelCase(this.props.name);
-    const prefix = this.props.org === 'mljs' ? 'ml-' : '';
+    const prefix = this.props.org === "mljs" ? "ml-" : "";
     const includes = {
       npmName: prefix + this.props.name,
       name: this.props.name,
@@ -74,7 +75,7 @@ export default class extends Generator {
       camelName,
     };
 
-    this.copyDestination(join(toCopy, '**'), '.', {
+    this.copyDestination(join(toCopy, "**"), ".", {
       globOptions: {
         globstar: true,
         deep: 3,
@@ -83,19 +84,21 @@ export default class extends Generator {
     });
 
     this.fs.copyTpl(
-      this.templatePath(join(templates, '*')),
-      this.destinationPath('.'),
+      this.templatePath(join(templates, "*")),
+      this.destinationPath("."),
       includes,
-      null,
+      undefined,
       {
         globOptions: {
           dot: true,
           onlyFiles: true,
         },
-      },
+        processDestinationPath: (path) => {
+          return path.split(/.ejs$/)[0];
+        },
+      }
     );
 
-    this.fs.copyTpl(join(__dirname, 'package'), 'package.json', includes);
-    this.addDevDependencies(deps);
+    await this.addDevDependencies(deps);
   }
 }
